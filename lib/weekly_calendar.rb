7a2ddc3ccadb1e53680ec07d858353434fd06ef8
Kieran Pilkington <kieran@katipo.co.nz>
2009-08-19 22:51:08 +1200

    Fixing 2 bugs in the weekly view - :business_hours needed to be passed into the weekly_calendar and week methods twice, and events from 12am to 1am were not visible.

--- lib/weekly_calendar.rb
+++ lib/weekly_calendar.rb
@@ -31,19 +31,21 @@ module WeeklyHelper
       @objects, @template, @options, @start_date, @end_date = objects, template, options, start_date, end_date
     
       if options[:business_hours] == "true" or options[:business_hours].blank?
-        @hours = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"]
+        @hours = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
         @header_row = "header_row"
         @day_row = "day_row"
         @grid = "grid"
         @start_hour = 6
         @end_hour = 20
+        @business_hours = true
       else
-        @hours = ["1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm","12am"]
+        @hours = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm"]
         @header_row = "full_header_row"
         @day_row = "full_day_row"
         @grid = "full_grid"
-        @start_hour = 1
+        @start_hour = 0
         @end_hour = 24
+        @business_hours = false
       end
     end
 
@@ -56,15 +58,13 @@ module WeeklyHelper
         concat(tag("div", :id => @grid))
           for day in @start_date..@end_date
             concat(tag("div", :id => @day_row))
-              @hours.each do |h|
-                for event in @objects
-                  if event.starts_at.strftime('%Y %j').to_s == day.strftime('%Y %j').to_s
-                    if event.starts_at.strftime('%H').to_i >= @start_hour and event.ends_at.strftime('%H').to_i <= @end_hour
-                      concat(tag("div", :id => "week_event", :style =>"left:#{left(event.starts_at,options[:business_hours])}px;width:#{width(event.starts_at,event.ends_at)}px;", :onclick => (options[:with_onclick] ? "location.href='/events/#{event.id}';" : nil)))
-                      truncate = truncate_width(width(event.starts_at,event.ends_at))
-                      yield(event,truncate)
-                      concat("</div>")
-                    end
+              for event in @objects
+                if event.starts_at.strftime('%Y %j').to_s == day.strftime('%Y %j').to_s
+                  if event.starts_at.strftime('%H').to_i >= @start_hour and event.ends_at.strftime('%H').to_i <= @end_hour
+                    concat(tag("div", :id => "week_event", :style =>"left:#{left(event.starts_at)}px;width:#{width(event.starts_at,event.ends_at)}px;", :onclick => (options[:with_onclick] ? "location.href='/events/#{event.id}';" : nil)))
+                    truncate = truncate_width(width(event.starts_at,event.ends_at))
+                    yield(event,truncate)
+                    concat("</div>")
                   end
                 end
               end 
@@ -100,19 +100,21 @@ module WeeklyHelper
       @template.concat(tag)
     end
 
-    def left(starts_at,business_hours)
-      if business_hours == "true" or business_hours.blank?
+    def left(starts_at)
+      if @business_hours
         minutes = starts_at.strftime('%M').to_f * 1.25
         hour = starts_at.strftime('%H').to_f - 6
       else
         minutes = starts_at.strftime('%M').to_f * 1.25
-        hour = starts_at.strftime('%H').to_f - 1
+        hour = starts_at.strftime('%H').to_f
       end
       left = (hour * 75) + minutes
     end
 
     def width(starts_at,ends_at)
       #example 3:30 - 5:30
+      return ((75 * (24 - starts_at.strftime('%H').to_i)) - 12) if ends_at.strftime('%H').to_i == 0
+
       start_hours = starts_at.strftime('%H').to_i * 60 # 3 * 60 = 180
       start_minutes = starts_at.strftime('%M').to_i + start_hours # 30 + 180 = 210
       end_hours = ends_at.strftime('%H').to_i * 60 # 5 * 60 = 300
